7c43e49a9fd100a946b8e577ab697c8b62983e05
Matthew Reidsma <reidsmam@gvsu.edu>
2015-11-13 16:55:31 -0500

    Added checkign for second search has because ProQuest does not do URLs by the books

--- summon2.0.js
+++ summon2.0.js
@@ -192,6 +192,15 @@ $(document).ready(function() {
   					return decodeURIComponent(pair[1]);
   				}
   			}
+        if(chunks[2]) {
+          var vars = chunks[2].split('&');
+        for (var i = 0; i < vars.length; i++) {
+          var pair = vars[i].split('=');
+          if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+          }
+        }
+        }
         console.log('Query variable %s not found', variable);
   		}
   	}
