2bc1d82073b3f4ae50853f6f984974e93bf96863
Matthew Reidsma <reidsmam@gvsu.edu>
2015-11-13 16:51:58 -0500

    Added code to save topic explorer entries that appear with searches

--- summon2.0.js
+++ summon2.0.js
@@ -102,16 +102,6 @@ $(document).ready(function() {
 
       // Watch the search results for changes, record new searches
 
-    var libMyScope = angular.element('html').scope();
-
-  // WATCH FOR RESULTS FEED CHANGES...
-    libMyScope.$watchCollection('feed', function(){
-      // give AngularJS time to update the DOM
-      setTimeout(function() { 
-        recordSearch();
-      }, 5000);
-      console.log('Scope.feed changed! - loading finished');
-  });
 
     function getParameterByName(name) {
         name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
@@ -216,10 +206,10 @@ $(document).ready(function() {
 
   function recordSearch() {
      // Record accurate Search usage
-   var searchQuery = libGetQueryVariable('s.q', window.location.search);
+   var searchQuery = libGetQueryVariable('q', window.location.href);
 
     if((typeof searchQuery === 'undefined') || (searchQuery == null)) {
-          searchQuery = libGetQueryVariable('q', window.location.href);
+          searchQuery = libGetQueryVariable('s.q', window.location.search);
     }
 
     console.log(searchQuery);
