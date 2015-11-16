3d93dab36191a6e7c5c9668c160ad5fd195e6b7b
Matthew Reidsma <reidsmam@gvsu.edu>
2015-11-16 11:55:58 -0500

    Added visualization tool for topic explorer data

---
+++ topics.php
@@ -0,0 +1,128 @@
+<?php
+
+// Show topic explorer entries with searches
+
+// File that defines database connection parameters
+include('config.php');
+
+$db = new mysqli($db_host, $db_user, $db_pass, $db_database);
+if ($db->connect_errno) {
+	printf("Connect failed: %s\n", $db->connect_error);
+	exit();
+}
+
+$search_results = $db->query("SELECT t.id, t.source, t.summary, t.title, s.search_query, s.instances
+							FROM topics as t, searches as s
+							WHERE s.id = t.id") or die($db->error);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Summon Searches with Topic Explorer Results</title>
+
+	<style>
+	table { 
+  width: 100%; 
+  border-collapse: collapse; 
+}
+
+tr:nth-of-type(odd) { 
+  background: #eee; 
+}
+th { 
+  background: #333; 
+  color: white; 
+  font-weight: bold; 
+}
+td, th { 
+  padding: 6px; 
+  border: 1px solid #ccc; 
+  text-align: left; 
+}
+
+@media 
+only screen and (max-width: 760px),
+(min-device-width: 768px) and (max-device-width: 1024px)  {
+
+	
+	table, thead, tbody, th, td, tr { 
+		display: block; 
+	}
+	
+	
+	thead tr { 
+		position: absolute;
+		top: -9999px;
+		left: -9999px;
+	}
+	
+	tr { border: 1px solid #ccc; }
+	
+	td { 
+		
+		border: none;
+		border-bottom: 1px solid #eee; 
+		position: relative;
+		padding-left: 50%; 
+	}
+	
+	td:before { 
+		
+		position: absolute;
+		
+		top: 6px;
+		left: 6px;
+		width: 45%; 
+		padding-right: 10px; 
+		white-space: nowrap;
+	}
+	
+	
+	td:nth-of-type(1):before { content: "Search"; }
+	td:nth-of-type(2):before { content: "Topic"; }
+	td:nth-of-type(3):before { content: "Source"; }
+	td:nth-of-type(4):before { content: "Summary"; }
+	
+}
+
+	</style>
+
+</head>
+<body>
+
+<table>
+	<thead>
+	  <tr>
+		<th>Search Query</th>
+		<th>Topic Title</th>
+		<th>Topic Source</th>
+		<th>Topic Summary</th>
+	  </tr>
+	</thead>
+	<tbody>
+
+<?php 
+
+if($search_results) {
+	while($row = $search_results->fetch_assoc()) {
+		echo '<tr>';
+			echo '<td>' . $row['search_query'] . '</td>';
+			echo '<td>' . $row['title'] . '</td>';
+			echo '<td>' . $row['source'] . '</td>';
+			echo '<td>' . $row['summary'] . '</td>';
+		echo '</tr>';
+
+	}
+}
+
+?>
+	</tbody>
+</table>
+
+
+
+
+
+</body>
+</html>
