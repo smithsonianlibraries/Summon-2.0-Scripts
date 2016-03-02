c9b983c78dd5980ec9ff1af12282cbab0b93bc8c
Matthew Reidsma <reidsmam@gvsu.edu>
2016-03-02 16:01:05 -0500

    Added cron script to purge match table

--- purge.php
+++ purge.php
@@ -5,6 +5,13 @@
 // File that defines database connection parameters
 include('config.php');
 
+
+$db = new mysqli($db_host, $db_user, $db_pass, $db_database);
+if ($db->connect_errno) {
+	printf("Connect failed: %s\n", $db->connect_error);
+	exit();
+}
+
 $now = strtotime('-1 hour');
 
 // SQL to remove all match table entries more than an hour old.
